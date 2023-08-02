5385a2b99b1aab6351c22bc4bbe36a408a207e2c
Anna <kanna1670@gmail.com>
2023-08-02 21:02:42 +0700

    Initial commit

--- src/Randoms.java
+++ src/Randoms.java
@@ -4,22 +4,21 @@ public class Randoms implements Iterable<Integer> {
     protected Random random;
     protected int min;
     protected int max;
+
     public Randoms(int min, int max) {
+        random = new Random();
         this.min = min;
         this.max = max;
-        Random random = new Random();
-        this.random = random;
     }
+
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
             @Override
             public boolean hasNext() {
-                if (random.equals(null)) {
-                    return false;
-                } else
-                    return true;
-            }
+                return true;
+        }
+
             @Override
             public Integer next() {
                 int diff = max - min;
